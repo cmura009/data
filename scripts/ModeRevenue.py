239691b73c9f1b59f382777ea0245f6755155df4
starglide <gemmine@gmail.com>
2017-04-13 17:59:13 -0400

    Add files via upload

---
+++ scripts/ModeRevenue.py
@@ -0,0 +1,24 @@
+from mrjob.job import MRJob
+
+
+class ModeRevenue(MRJob):
+  
+    # This is bad practice - make sure you know what you're doing!g
+    count = 0
+
+    def mapper(self, _, line):
+        line_cols = line.split(',')
+        yield line_cols[3], float(line_cols[2])
+
+    def combiner(self, mode, counts):
+        yield mode, sum(counts)
+
+    def reducer(self, mode, counts):
+        self.count += 1
+        
+        if self.count <= 5:
+          yield mode, '${:,.2f}'.format(sum(counts))
+
+
+if __name__ == '__main__':
+    ModeRevenue.run()
