ee698a07ed72b027c0993869d4bc1362c79a9ce3
starglide <gemmine@gmail.com>
2017-04-14 12:09:46 -0400

    Update CategoryCount.py

--- scripts/CategoryCount.py
+++ scripts/CategoryCount.py
@@ -1,16 +1,24 @@
+"""
+ Python script to find the number of sales transactions for each product category
+ using Map-Reduce framework (mapper, combiner, and reducer functions) with mrjob package
+ 4/14/17
+"""
 from mrjob.job import MRJob
 
-
 class CategoryCount(MRJob):
+# each input lines consists of city, productCategory, price, and paymentMode
 
     def mapper(self, _, line):
+        # create a key-value pair with key: product category and value:1
         line_cols = line.split(',')
         yield line_cols[1], 1
 
     def combiner(self, category, counts):
+        # consolidates all key-value pairs of mapper function (performed at mapper nodes)
         yield category, sum(counts)
 
     def reducer(self, category, counts):
+        # final consolidation of key-value pairs at reducer nodes
         yield category, sum(counts)
 
 
